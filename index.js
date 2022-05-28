98199bb5d5289c0bc8976bc29eeba43e8c9b0559
nati2323 <nati2.man.3@gmail.com>
2022-05-28 15:42:47 +0300

    Update linters file

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 function appear() {
-  var readMoreButton = document.getElementById('read-more-button');
-  var additionalSingers = document.querySelectorAll('.additional-singers-article');
- 
+  const readMoreButton = document.getElementById('read-more-button');
+  const additionalSingers = document.querySelectorAll('.additional-singers-article');
+
+  // var arrowForward = document.getElementById('arrowForward');
+  // var arrowBackward = document.getElementById('arrowBackward').innerHTML('22');
+
   for (let i = 1; i < additionalSingers.length; i++) {
     if (additionalSingers[i].style.display === 'flex') {
       additionalSingers[i].style.display = 'none';
       readMoreButton.innerHTML = 'Read More >>';
-      console.log('if');
-    }else {
+    } else {
       additionalSingers[i].style.display = 'flex';
       readMoreButton.innerHTML = '<< Read Less';
     }
@@ -15,12 +17,10 @@ function appear() {
 }
 
 function mobileNav() {
-  var mobilNavUl = document.querySelector('.mobile-nav-ul');
+  const mobilNavUl = document.querySelector('.mobile-nav-ul');
   if (mobilNavUl.style.display === 'block') {
-    console.log('1');
     mobilNavUl.style.display = 'none';
-  }else {
-    console.log('2');
+  } else {
     mobilNavUl.style.display = 'block';
   }
 }
