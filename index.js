ddab90e9391c22a61928d0c2356bc499ec4b01f7
nati2323 <99729850+nati2323@users.noreply.github.com>
2022-05-28 15:06:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-  let readMoreButton = document.getElementById('read-more-button');
+function appear() {  
+let readMoreButton = document.getElementById('read-more-button');
   let additionalSingers = document.querySelectorAll('.additional-singers-article');
  
   for (let i = 1; i < additionalSingers.length; i++) {
@@ -11,3 +12,4 @@
       readMoreButton.innerHTML = '<< Read Less';
     }
   }
+}
